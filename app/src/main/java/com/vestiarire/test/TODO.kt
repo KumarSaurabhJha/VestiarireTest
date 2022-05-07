a1d97f5f77f6e148640577b210c8c6bc35ab4aaa
saurabhkumar <k.saurabh@vereinigte-hagel.de>
2022-05-07 13:11:24 +0200

    Add list of TODOs to continue working in future

---
+++ app/src/main/java/com/vestiarire/test/TODO.kt
@@ -0,0 +1,16 @@
+package com.vestiarire.test
+
+class TODO {
+
+    /**
+     * TODO:
+     * 1. Run the Rest request run to get sample JSON
+     * 2. Create MOdel Structure for the JSON
+     * 3. Create UI layer for Screen (RV, Screens etc.)
+     * 4. Support Offline Functionality for Weather Map
+     * 5. Write Test Case
+     * 6. Readme and Comments
+     * 7. Formatting
+     * 8. Delete this class
+     */
+}
